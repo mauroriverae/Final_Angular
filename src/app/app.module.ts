bcaa0f3c596e78bc8c843d1b2d23f48935d08e65
Mauro Rivera <mauororiverae@gmail.com>
2022-10-17 17:21:44 -0300

    Agregando lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IceCreamListComponent } from './ice-cream-list/ice-cream-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IceCreamListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ice-cream-list/ice-cream-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ice-cream-list',
+  templateUrl: './ice-cream-list.component.html',
+  styleUrls: ['./ice-cream-list.component.scss']
+})
+export class IceCreamListComponent implements OnInit {
+  ice = {
+    "quantity" : "1/4kg",
+    "price" : 600,
+    "stock" :  5,
+    "image" : "src/assets/img/ice.jpg" 
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
