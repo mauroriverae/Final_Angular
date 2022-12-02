3b9b9c54e8ec35e2d1934e6129b752577d242b88
Mauro Rivera <mauroriverae@gmail.com>
2022-12-02 15:10:00 -0300

    final

--- src/app/sabores-c/sabores-c.component.ts
+++ src/app/sabores-c/sabores-c.component.ts
@@ -10,7 +10,7 @@ export class SaboresCComponent {
   saboresList() :void{
     let sabores = ["Chocolates", "Dulce de leche", "Americana", "Cereza", "Chocolate Italiano ",]
     for( let sabor of sabores){
-      console.log(`<li>${sabor}</li>`);
+      alert(sabor); //hacer modal 
     }
   }
 }
